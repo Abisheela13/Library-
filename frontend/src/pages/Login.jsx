06494d8a4ffc303713c17bbb7c9e6bc635a066be
Abisheela <abisheelaalagumalai@gmail.com>
2026-05-12 10:00:14 +0530

    frontend auth fixed

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,45 +1,116 @@
-
 import React,{useState} from "react";
 import api from "../services/api";
 import { useNavigate, Link } from "react-router-dom";
 
 function Login(){
+
  const nav = useNavigate();
- const [data,setData] = useState({email:"",password:""});
+
+ const [data,setData] = useState({
+  email:"",
+  password:""
+ });
 
  const login = async()=>{
-  const res = await api.post("/login",data);
 
-  localStorage.setItem("token",res.data.token);
-  localStorage.setItem("role",res.data.user.role);
+  try{
 
-  if(res.data.user.role==="ADMIN"){
-   nav("/admin");
-  }else{
-   nav("/user");
+   const res = await api.post("/login",data);
+
+   localStorage.setItem("token",res.data.token);
+
+   localStorage.setItem("role",res.data.user.role);
+
+   if(res.data.user.role==="ADMIN"){
+
+    nav("/admin");
+
+   }else{
+
+    nav("/user");
+   }
+
+  }catch(err){
+
+   console.log(err);
+
+   alert(
+    err.response?.data?.msg || "Login Failed"
+   );
   }
  }
 
  return(
+
   <div className="container py-5">
+
    <div className="row justify-content-center">
+
     <div className="col-md-5">
+
      <div className="card shadow p-4">
-      <h3 className="text-center mb-4">Library Login</h3>
 
-      <input className="form-control mb-3" placeholder="Email"
-      onChange={(e)=>setData({...data,email:e.target.value})}/>
+      <h3 className="text-center mb-4">
+       Library Login
+      </h3>
 
-      <input type="password" className="form-control mb-3" placeholder="Password"
-      onChange={(e)=>setData({...data,password:e.target.value})}/>
+      <input
+      className="form-control mb-3"
+      placeholder="Email"
+      value={data.email}
+      onChange={(e)=>
+      setData({
+       ...data,
+       email:e.target.value
+      })}
+      />
 
-      <button className="btn btn-dark w-100" onClick={login}>Login</button>
+      <input
+      type="password"
+      className="form-control mb-3"
+      placeholder="Password"
+      value={data.password}
+      onChange={(e)=>
+      setData({
+       ...data,
+       password:e.target.value
+      })}
+      />
 
-      <Link to="/register" className="mt-3 text-center">New User? Register</Link>
+      <button
+      className="btn btn-dark w-100"
+      onClick={login}
+      >
+       Login
+      </button>
+
+      <button
+      className="btn btn-warning w-100 mt-2"
+      onClick={()=>{
+       setData({
+        email:"abi@gmail.com",
+        password:"123456"
+       });
+      }}
+      >
+       Fill Admin Login
+      </button>
+
+      <Link
+      to="/register"
+      className="mt-3 text-center"
+      >
+       New User? Register
+      </Link>
+
      </div>
+
     </div>
+
    </div>
+
   </div>
  )
 }
+
 export default Login;
